d444bababe4b32a1e9545a5dbdbe2041105813de
Oooch <azzindahouse@gmail.com>
2018-01-01 15:13:32 +0000

    First reasonable beta

--- Binance API 2/VolumeInfo.Designer.cs
+++ Binance API 2/VolumeInfo.Designer.cs
@@ -29,10 +29,11 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.coindatagridview = new System.Windows.Forms.DataGridView();
             this.RefreshButton = new System.Windows.Forms.Button();
+            this.alerticon = new System.Windows.Forms.NotifyIcon(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.coindatagridview)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,51 +49,57 @@
             this.coindatagridview.AllowUserToOrderColumns = true;
             this.coindatagridview.AllowUserToResizeColumns = false;
             this.coindatagridview.AllowUserToResizeRows = false;
-            this.coindatagridview.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.coindatagridview.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.coindatagridview.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
-            this.coindatagridview.BackgroundColor = System.Drawing.SystemColors.ControlText;
             this.coindatagridview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.coindatagridview.DefaultCellStyle = dataGridViewCellStyle3;
-            this.coindatagridview.Location = new System.Drawing.Point(12, 12);
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.coindatagridview.DefaultCellStyle = dataGridViewCellStyle1;
+            this.coindatagridview.Location = new System.Drawing.Point(-4, -2);
             this.coindatagridview.Name = "coindatagridview";
             this.coindatagridview.ReadOnly = true;
             this.coindatagridview.RowHeadersVisible = false;
             this.coindatagridview.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.coindatagridview.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
-            this.coindatagridview.Size = new System.Drawing.Size(743, 409);
+            this.coindatagridview.Size = new System.Drawing.Size(744, 587);
             this.coindatagridview.TabIndex = 3;
             this.coindatagridview.SelectionChanged += new System.EventHandler(this.coindatagridview_SelectionChanged);
+            this.coindatagridview.Sorted += new System.EventHandler(this.coindatagridview_Sorted);
             // 
             // RefreshButton
             // 
             this.RefreshButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.RefreshButton.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.RefreshButton.Location = new System.Drawing.Point(12, 447);
+            this.RefreshButton.Location = new System.Drawing.Point(12, 534);
             this.RefreshButton.Name = "RefreshButton";
             this.RefreshButton.Size = new System.Drawing.Size(117, 36);
             this.RefreshButton.TabIndex = 4;
             this.RefreshButton.Text = "Refresh";
             this.RefreshButton.UseVisualStyleBackColor = true;
+            this.RefreshButton.Visible = false;
             this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // alerticon
+            // 
+            this.alerticon.Text = "notifyIcon1";
+            this.alerticon.Visible = true;
             // 
             // VolumeInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlText;
-            this.ClientSize = new System.Drawing.Size(767, 516);
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(724, 582);
             this.Controls.Add(this.RefreshButton);
             this.Controls.Add(this.coindatagridview);
             this.Name = "VolumeInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Coin Volume";
             ((System.ComponentModel.ISupportInitialize)(this.coindatagridview)).EndInit();
             this.ResumeLayout(false);
@@ -103,6 +110,7 @@
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.DataGridView coindatagridview;
         private System.Windows.Forms.Button RefreshButton;
+        private System.Windows.Forms.NotifyIcon alerticon;
     }
 }
 
--- Binance API 2/VolumeInfo.cs
+++ Binance API 2/VolumeInfo.cs
@@ -2,6 +2,7 @@
 using Binance.Market;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -19,15 +20,16 @@ namespace Binance_API_2
             TableInit();
             RefreshButton.Visible = false;
             FontSet();
-            SetupCoinData();
             GetVolumes();
-            coindatagridview.DataSource = CoinData;
+            
         }
         //1200 Requests a minute
         //10 Orders a second
         //100,000 Orders a day
         DataTable CoinData = new DataTable();
-
+        DataTable CoinData2 = new DataTable();
+        private IEnumerable<SymbolPrice> coinprices;
+        private IEnumerable<SymbolPrice> _pricesArray;
         public void TableInit()
         {
             CoinData.Columns.Add("Symbol", typeof(string));
@@ -36,60 +38,89 @@ namespace Binance_API_2
             CoinData.Columns.Add("NowPercent", typeof(decimal));
             CoinData.Columns.Add("15MinPercent", typeof(decimal));
             CoinData.Columns.Add("GreenCandles", typeof(decimal));
+            CoinData2.Columns.Add("Symbol", typeof(string));
+            CoinData2.Columns.Add("Scanned", typeof(DateTime));
+            CoinData2.Columns.Add("8HrAv", typeof(int));
+            CoinData2.Columns.Add("NowPercent", typeof(decimal));
+            CoinData2.Columns.Add("15MinPercent", typeof(decimal));
+            CoinData2.Columns.Add("GreenCandles", typeof(decimal));
         }
 
-        private async void SetupCoinData()
+        private async Task<DataTable> SetupCoinData()
         {
-            BinanceApi api = new BinanceApi();
-            IEnumerable<SymbolPrice> coinprices = await api.GetPricesAsync(CancellationToken.None); // Grab all coin symbols
-            pricesArray = coinprices.ToArray()
-                .Where(o => o.Symbol == "BTCUSDT" || o.Symbol == "ETHBTC" || o.Symbol.EndsWith("ETH"));
-        }
-
-        private IEnumerable<SymbolPrice> pricesArray;
-        public async Task<DataTable> GetVolumes()
-        {
-            
-            BinanceApi api = new BinanceApi();
-            
-            foreach (SymbolPrice pricer in pricesArray)
+            if (CoinData.Rows.Count == 0)
             {
-                IEnumerable<Candlestick> eighthourcandles = await api.GetCandlesticksAsync(pricer.Symbol, CandlestickInterval.Minutes_5, 0, 0L, 0L, CancellationToken.None);
-                IOrderedEnumerable<Candlestick> candlearray = eighthourcandles.OrderByDescending(e => e.CloseTime);
-                decimal averagevolumeeighthours = candlearray.Average(candlestick => candlestick.Volume);
-                decimal fifteenminavg = (candlearray.ElementAt(1).Volume + candlearray.ElementAt(2).Volume +
-                                         candlearray.ElementAt(0).Volume) / 3;
-                DataRow row = CoinData.NewRow();
-                row[0] = pricer.Symbol;
-                row[1] = DateTime.UtcNow;
-                row[2] = Decimal.Round(averagevolumeeighthours, MidpointRounding.AwayFromZero); 
-                row[3] = PercentGive(candlearray.First().Volume, averagevolumeeighthours);
-                row[4] = PercentGive(fifteenminavg, averagevolumeeighthours);
-                int greencandles = 0;
-                int i = 0;
-                while (i != 9)
+                BinanceApi api = new BinanceApi();
+               coinprices = await api.GetPricesAsync(CancellationToken.None); // Grab all coin symbols
+                _pricesArray = coinprices.ToArray().Where(o =>
+                    o.Symbol == "BTCUSDT" || o.Symbol == "ETHBTC" || o.Symbol.EndsWith("ETH"));
+                foreach (SymbolPrice price in _pricesArray)
                 {
-                    if (candlearray.ElementAt(i).Close > candlearray.ElementAt(i + 1).Close)
-                    {
-                        greencandles++;
-                    }
-                    i++;
+                    DataRow row = CoinData.NewRow();
+                    row[0] = price.Symbol;
+                    row[1] = DateTime.UtcNow;
+                    CoinData.Rows.Add(row);
                 }
-                row[5] = greencandles;
-                CoinData.Rows.Add(row);
-                DataViewColours();
             }
-            Flashing.FlashWindowEx(this);
-            System.Media.SystemSounds.Beep.Play();
-            RefreshButton.Visible = true;
             return CoinData;
         }
 
+        
+        public async Task<DataTable> GetVolumes()
+        {
+            BinanceApi api = new BinanceApi();
+            await SetupCoinData();
+            while (true)
+            {
+                foreach (DataRow pricer in CoinData.Rows)
+                {
+                    IEnumerable<Candlestick> eighthourcandles = await api.GetCandlesticksAsync(pricer[0].ToString(),
+                        CandlestickInterval.Minutes_5, 0, 0L, 0L, CancellationToken.None);
+                    IOrderedEnumerable<Candlestick> candlearray = eighthourcandles.OrderByDescending(e => e.CloseTime);
+                    decimal averagevolumeeighthours = candlearray.Average(candlestick => candlestick.Volume);
+                    decimal fifteenminavg = (candlearray.ElementAt(1).Volume + candlearray.ElementAt(2).Volume +
+                                             candlearray.ElementAt(0).Volume) / 3;
+                    DataRow row = CoinData.Rows.Cast<DataRow>().First(o => o[0].ToString() == pricer[0].ToString());
+                    row[1] = DateTime.UtcNow;
+                    row[2] = Decimal.Round(averagevolumeeighthours, MidpointRounding.AwayFromZero);
+                    row[3] = PercentGive(candlearray.First().Volume, averagevolumeeighthours);
+                    row[4] = PercentGive(fifteenminavg, averagevolumeeighthours);
+                    int greencandles = 0;
+                    int i = 0;
+                    while (i != 9)
+                    {
+                        if (candlearray.ElementAt(i).Close > candlearray.ElementAt(i + 1).Close)
+                        {
+                            greencandles++;
+                        }
+                        i++;
+                    }
+                    row[5] = greencandles;
+                    CoinData.DefaultView.RowFilter = "NowPercent > 0";
+                    //var filtered = CoinData.Select().Where(o => Convert.ToDecimal(o[3].ToString()) > Convert.ToDecimal(o[4].ToString()));
+                    DataViewColours();
+                    coindatagridview.DataSource = CoinData;
+                    coindatagridview.Sort(coindatagridview.Columns[3], ListSortDirection.Descending);
+                    
+                }
+
+                
+
+
+
+            }
+            
+            return CoinData;
+        }
+
+        //private async Task<DataRow> RowUpdater()
+        //{
+            
+        //}
 
         private async void RefreshButton_Click(object sender, EventArgs e)
         {
             RefreshButton.Visible = false;
-            CoinData.Rows.Clear();
             await GetVolumes();
         }
 
@@ -106,19 +137,84 @@ namespace Binance_API_2
                 CellCheck(viewRow, 4);
                 CellCheck(viewRow, 5);
             }
+            //int i = coindatagridview.Rows.Count - 1;
+            //while (i >= 0)
+            //{
+            //    CellCheck(coindatagridview.Rows[i], 3);
+            //    CellCheck(coindatagridview.Rows[i], 4);
+            //    CellCheck(coindatagridview.Rows[i], 5);
+            //    i--;
+            //}
+            //foreach (DataGridViewRow viewRow in coindatagridview2.Rows)
+            //{
+            //    CellCheck(viewRow, 3);
+            //    CellCheck(viewRow, 4);
+            //    CellCheck(viewRow, 5);
+            //}
+
         }
 
-        private void CellCheck(DataGridViewRow viewRow, int i)
+        
+
+
+        private decimal CellCheck(DataGridViewRow viewRow, int i)
         {
-            if (Convert.ToDecimal(viewRow.Cells[i].Value) > 5)
+            if (viewRow.Cells[i].Value.ToString() == "")
             {
-                viewRow.Cells[i].Style.BackColor = Color.Green;
-                viewRow.Cells[i].Style.ForeColor = Color.White;
+                return 0;
             }
-            if (Convert.ToDecimal(viewRow.Cells[i].Value) > 5)
+            else
             {
-                viewRow.Cells[i].Style.BackColor = Color.Green;
-                viewRow.Cells[i].Style.ForeColor = Color.White;
+                if (i == 5)
+                {
+                    if (Convert.ToDecimal(viewRow.Cells[i].Value) >= 4)
+                    {
+                        viewRow.Cells[i].Style.BackColor = Color.LightGreen;
+                        viewRow.Cells[i].Style.ForeColor = Color.Black;
+                    }
+                    if (Convert.ToDecimal(viewRow.Cells[i].Value) >= 6)
+                    {
+                        viewRow.Cells[i].Style.BackColor = Color.Green;
+                        viewRow.Cells[i].Style.ForeColor = Color.White;
+                    }
+                    if (Convert.ToDecimal(viewRow.Cells[i].Value) < 4)
+                    {
+                        viewRow.Cells[i].Style.BackColor = Color.White;
+                        viewRow.Cells[i].Style.ForeColor = Color.Black;
+                    }
+                }
+                else
+                {
+                    if (Convert.ToDecimal(viewRow.Cells[i].Value) >= 0)
+                    {
+                        viewRow.Cells[i].Style.BackColor = Color.LightGreen;
+                        viewRow.Cells[i].Style.ForeColor = Color.Black;
+                  
+                    }
+                    if (Convert.ToDecimal(viewRow.Cells[i].Value) >= 5)
+                    {
+                        viewRow.Cells[i].Style.BackColor = Color.Green;
+                        viewRow.Cells[i].Style.ForeColor = Color.White;
+                    }
+                    if (Convert.ToDecimal(viewRow.Cells[i].Value) < 0)
+                    {
+                        viewRow.Cells[i].Style.BackColor = Color.LightCoral;
+                        viewRow.Cells[i].Style.ForeColor = Color.Black;
+                       
+                    }
+                    if (Convert.ToDecimal(viewRow.Cells[i].Value) <= -10)
+                    {
+                        viewRow.Cells[i].Style.BackColor = Color.Red;
+                        viewRow.Cells[i].Style.ForeColor = Color.Black;
+                    }
+                    if (Convert.ToDecimal(viewRow.Cells[i].Value) <= -30)
+                    {
+                        viewRow.Cells[i].Style.BackColor = Color.DarkRed;
+                        viewRow.Cells[i].Style.ForeColor = Color.White;
+                    }
+
+                }
+                return Convert.ToDecimal(viewRow.Cells[i].Value);
             }
         }
 
@@ -135,10 +231,29 @@ namespace Binance_API_2
             foreach (Control control in Controls)
             {
                 {
-                    control.Font = new Font("Arial", 12, FontStyle.Bold);
-                    //control.BackColor = Color.Black;
-                    //control.ForeColor = Color.White;
+                    control.Font = new Font("Arial", 10, FontStyle.Bold);
                 }
+            }
+        }
+
+        private void coindatagridview_Sorted(object sender, EventArgs e)
+        {
+            
+          
+            DataViewColours();
+        }
+
+
+
+        private ListSortDirection SortOrder(SortOrder sortOrder)
+        {
+            if (sortOrder.ToString() == "Descending")
+            {
+                return ListSortDirection.Descending;
+            }
+            else
+            {
+                return ListSortDirection.Ascending;
             }
         }
     }
